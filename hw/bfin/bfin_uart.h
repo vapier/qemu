ed5bb8eaaa89c432d6c94465eb00c91a220f6b82
Mike Frysinger <vapier@gentoo.org>
2012-07-13 22:05:42 +0100

    Blackfin: initial set of models
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

---
+++ hw/bfin/bfin_uart.h
@@ -0,0 +1,17 @@
+/*
+ * Blackfin Universal Asynchronous Receiver/Transmitter (UART) model.
+ * For "old style" UARTs on BF53x/etc... parts.
+ *
+ * Copyright 2007-2014 Mike Frysinger
+ * Copyright 2007-2011 Analog Devices, Inc.
+ *
+ * Licensed under the Lesser GPL 2 or later.
+ */
+
+#ifndef DV_BFIN_UART_H
+#define DV_BFIN_UART_H
+
+/* UART_LCR */
+#define DLAB (1 << 7)
+
+#endif
