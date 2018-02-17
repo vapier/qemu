4e2a1c1cc11f33e4fbb5a3bc92f2df3d9f19b13e
Mike Frysinger <vapier@gentoo.org>
2018-02-16 20:55:59 -0500

    linux-user: make getcpu optional
    Not all arches implement this, and the kernel doesn't require them to.
    Add ifdef logic to disable it when not available.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -301,8 +301,10 @@ _syscall3(int, sys_sched_getaffinity, pid_t, pid, unsigned int, len,
 #define __NR_sys_sched_setaffinity __NR_sched_setaffinity
 _syscall3(int, sys_sched_setaffinity, pid_t, pid, unsigned int, len,
           unsigned long *, user_mask_ptr);
+#ifdef TARGET_NR_getcpu
 #define __NR_sys_getcpu __NR_getcpu
 _syscall3(int, sys_getcpu, unsigned *, cpu, unsigned *, node, void *, tcache);
+#endif
 _syscall4(int, reboot, int, magic1, int, magic2, unsigned int, cmd,
           void *, arg);
 _syscall2(int, capget, struct __user_cap_header_struct *, header,
@@ -9413,6 +9415,7 @@ static abi_long do_syscall1(void *cpu_env, int num, abi_long arg1,
 
             return get_errno(sys_sched_setaffinity(arg1, mask_size, mask));
         }
+#ifdef TARGET_NR_getcpu
     case TARGET_NR_getcpu:
         {
             unsigned cpu, node;
@@ -9430,6 +9433,7 @@ static abi_long do_syscall1(void *cpu_env, int num, abi_long arg1,
             }
         }
         return ret;
+#endif
     case TARGET_NR_sched_setparam:
         {
             struct sched_param *target_schp;
